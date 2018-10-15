07089f3cce36f1bee62d785c71b3198d4d6a0462
wyswill <1151373571@qq.com>
2018-10-15 11:33:02 +0800

    更新bug

--- 题目/app.js
+++ 题目/app.js
@@ -1,43 +1,3 @@
-// const matrix = [
-//         [1, 5, 3],
-//         [3, 4, 5],
-//         [3, 6, 5],
-//         [5, 6, 7],
-//         [8, 6, 7]
-//     ],
-//     lines = [
-//         [2, 0, 0, 1, 2],
-//         [1, 2, 2, 1, 0],
-//         [0, 1, 1, 2, 0]
-//     ],
-//     rewards = [{
-//             elem: 3,
-//             num: 3,
-//             reward: 2
-//         }, {
-//             elem: 3,
-//             num: 4,
-//             reward: 5
-//         }, {
-//             elem: 3,
-//             num: 5,
-//             reward: 7
-//         },
-//         {
-//             elem: 5,
-//             num: 3,
-//             reward: 3
-//         }, {
-//             elem: 5,
-//             num: 4,
-//             reward: 6
-//         }, {
-//             elem: 5,
-//             num: 5,
-//             reward: 8
-//         }
-//     ];
-
 const matrix = [
         [1, 5, 3],
         [3, 4, 5],
@@ -46,8 +6,9 @@ const matrix = [
         [8, 6, 7]
     ],
     lines = [
-        [0, 1, 2, 2, 0],
-        [1, 2, 0, 1, 2]
+        [2, 0, 0, 1, 2],
+        [1, 2, 2, 1, 0],
+        [0, 1, 1, 2, 0]
     ],
     rewards = [{
             elem: 3,
@@ -85,8 +46,7 @@ function calc(matrix, lines, rewards) {
             num: 0,
             reward: 0
         },
-        line_res = [],
-        counst = 0;
+        line_res = [];
     //得到线数据 
     for (let i = 0; i < lines.length; i++) {
         for (let j = 0; j < lines[i].length; j++) {
@@ -94,6 +54,7 @@ function calc(matrix, lines, rewards) {
         }
 
         resObj.lineId = i;
+        let counst = 0;
         line_res.forEach((ele, index) => {
             if (ele == line_res[index + 1]) {
                 counst++;
