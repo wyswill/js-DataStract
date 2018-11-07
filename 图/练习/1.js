cb8d4a6fada3856b3b988c1222988d8b6ff71d85
Mac <mac@MacdeMacBook-Pro.local>
2018-11-07 17:35:41 +0800

    图

---
+++ 图/练习/1.js
@@ -0,0 +1,38 @@
+/**
+ * @class Graph
+ * @description 图类
+ */
+class Graph {
+    constructor(v = 10) {
+        this.vertices = v;
+        this.edges = 0;//边
+        this.adj = [];
+        // 创建矩阵
+        for (let i = 0; i < this.vertices; i++) {
+            this.adj[i] = new Array('');
+        }
+    }
+    addEdge(v, w) {
+        this.adj[v].push(w);
+        this.adj[w].push(v);
+        this.edges++;
+    }
+    toString() {
+        for (var i = 0; i < this.vertices; ++i) {
+            // console.log(i + "->");
+            for (var j = 0; j < this.vertices; ++j) {
+                if (this.adj[i][j] != undefined) console.log(this.adj[i][j] + ' ');
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
